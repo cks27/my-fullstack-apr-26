c494e4f23ce2666083cf5fedf8dfd6d68eba1544
cks <chinmayksah@gmail.com>
2026-07-07 21:59:05 +0530

    usestate

--- my-practice/MY-LLD-Dev1/React/class-3/state-demo/src/components/Product.jsx
+++ my-practice/MY-LLD-Dev1/React/class-3/state-demo/src/components/Product.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Product = (props) => {
+  const [name, setName] = useState(props.name);
+  const productClickHandler = () => {
+    setName('Anonymous');
+    console.log(name)
+  }
   return (
-    <article>
-        <h4>Name: {props.name}</h4>
+    <article onClick={productClickHandler}>
+        <h4>Name: {name}</h4>
         <h5>Price: {props.price}</h5>
         <p>This is product details</p>
     </article>
