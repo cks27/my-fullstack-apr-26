8b858e428d227fb5e5bf8af98b1cb131e432e98a
cks <chinmayksah@gmail.com>
2026-08-24 16:32:57 +0530

    express demo

--- my-practice/MY-LLD-Dev1/MERN/class-2/express-demo/index.js
+++ my-practice/MY-LLD-Dev1/MERN/class-2/express-demo/index.js
@@ -2,4 +2,45 @@ const express = require('express');
 
 const app = express();
 
-console.log(app);
+// console.log(app);
+
+// express will execue this app.use for any incoming request
+// app.use((req, res) => {
+//     console.log('inside my app use');
+//     res.send('Hello from server');
+// })
+
+app.get('/', (req, res) => {
+    res.send('home route')
+});
+
+app.get('/cat', (req, res) => {
+    res.send('cat route')
+});
+
+app.get('/login', (req, res) => {
+    res.send('login route')
+});
+
+app.get('/users', (req, res) => {
+    res.send('GET - users route')
+});
+
+app.post('/users', (req, res) => {
+    res.send('post - users route')
+});
+
+app.get('/r/:subredit', (req, res) => {
+    const {subredit} = req.params;
+    res.send(`subredit: ${subredit}`);
+})
+
+app.get('/products', (req, res) => {
+    const { sort ='ASC' } = req.query;
+    res.send(`Listing products : ${sort}`);
+})
+
+
+app.listen(3000, () => {
+    console.log('server started on port 3000')
+})
